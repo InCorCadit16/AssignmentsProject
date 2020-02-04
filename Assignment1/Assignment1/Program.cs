268872a74c577a5a9f0e15025a099d9b290d3bd7
Alexandru Rosca <alexandru.rosca@amdaris.com>
2020-02-04 14:59:43 +0200

    resolved conflict while mergin text-branch

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -9,13 +9,8 @@ namespace Assignment1
             string s = "Hello World!";
             Console.WriteLine(s);
 
-<<<<<<< HEAD
-            string s1 = "String from master";
-            Console.WriteLine(s1);
-=======
             string str = "String from text_branch";
             Console.WriteLine(str);
->>>>>>> text_branch
         }
     }
 }
