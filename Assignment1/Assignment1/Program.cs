a556e96607e40a49fefc38af111809d6dc34d1ce
Alexandru Rosca <alexandru.rosca@amdaris.com>
2020-02-04 14:57:22 +0200

    Added string s1

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -6,7 +6,11 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string s = "Hello World!";
+            Console.WriteLine(s);
+
+            string s1 = "String from master";
+            Console.WriteLine(s1);
         }
     }
 }
