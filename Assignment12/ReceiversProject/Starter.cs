e7c5350d38ef464d525bde498275c0e37e138319
InCorCadit16 <incorcadit16@gmail.com>
2020-02-22 00:58:04 +0200

    Assignment 13: finished static version

--- Assignment12/ReceiversProject/Starter.cs
+++ Assignment12/ReceiversProject/Starter.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Windows;
 using PublisherSolution;
 
 
--- Assignment13/Assignment13/Runtime.cs
+++ Assignment13/Assignment13/Runtime.cs
@@ -14,7 +14,7 @@ namespace Assignment13
         {
             //Console.Write("Write first dir: ");
             //string Path1 = Console.ReadLine();
-            string Path1 = @"C:\Users\alexandru.rosca\source\repos\AssignmentsProject\Assignment13\Assignment13\obj\Debug\netcoreapp3.1\dir1";
+            string Path1 = @"dir1";
             Dir1 = new DirectoryInfo(Path1);
 
             Watcher1 = new FileSystemWatcher();
@@ -30,7 +30,7 @@ namespace Assignment13
 
             //Console.Write("Write second dir: ");
             //string Path2 = Console.ReadLine();
-            string Path2 = @"C:\Users\alexandru.rosca\source\repos\AssignmentsProject\Assignment13\Assignment13\obj\Debug\netcoreapp3.1\dir2";
+            string Path2 = @"dir2";
             Dir2 = new DirectoryInfo(Path2);
 
             int i = 1;
@@ -47,57 +47,30 @@ namespace Assignment13
 
         static void OnChanged(object source, FileSystemEventArgs args)
         {
-            FileInfo ChangedFile = new FileInfo(args.FullPath);
-            FileStream ReadStream = null;
-            
-            try {
-                ReadStream = ChangedFile.OpenRead();
-                using FileStream WriteStream = File.OpenWrite($"{Dir2.FullName}\\{args.Name}");
-                {
-                    int offset = 0;
-                    int count = 1024;
-                    byte[] buffer = new byte[count];
-                    while ((count = ReadStream.Read(buffer, offset, count)) > 0)
-                        WriteStream.Write(buffer, offset, count);
-                }
-            } catch(IOException ioe)
-            {
-                Console.WriteLine(ioe.Message);
-                ReadStream?.Close();
-            }
-            
+            File.Copy($"{args.FullPath}", $"{args.FullPath.Replace("dir1", "dir2")}", true);
+
+            Console.WriteLine($"{args.Name} changed");
         }
 
         static void OnCreated(object source, FileSystemEventArgs args)
         {
-            FileInfo CreatedFile = new FileInfo(args.FullPath);
-            File.Copy($"{Dir1.FullName}\\{args.Name}", $"{Dir2.FullName}\\{args.Name}", true);
+            File.Copy($"{args.FullPath}", $"{args.FullPath.Replace("dir1","dir2")}", true);
+
             Console.WriteLine($"{args.Name} copied");
         }
 
         static void OnRenamed(object source, RenamedEventArgs args)
-        {
-            FileInfo CreatedFile = new FileInfo(args.FullPath);
+        { 
             File.Move($"{Dir2.FullName}\\{args.OldName}", $"{Dir2.FullName}\\{args.Name}");
+
             Console.WriteLine($"{args.OldName} renamed to {args.Name}");
         }
 
         static void OnDeleted(object source, FileSystemEventArgs args)
         {
             new FileInfo($"{Dir2.FullName}\\{args.Name}").Delete();
+
             Console.WriteLine($"{args.Name} deleted");
-        }
-
-
-        static string CheckMD5(string filename)
-        {
-            using (var md5 = MD5.Create())
-            {
-                using (var stream = File.OpenRead(filename))
-                {
-                    return md5.ComputeHash(stream).ToString();
-                }
-            }
         }
     }
 }
