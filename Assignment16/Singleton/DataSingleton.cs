91a7682076be8444d776cda41aa570f8bd701854
Alexandru Rosca <alexandru.rosca@amdaris.com>
2020-02-25 18:45:18 +0200

    sigleton update

---
+++ Assignment16/Singleton/DataSingleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment16.Singleton
+{
+    public class DataSingleton
+    {
+        static readonly Lazy<DataSingleton> _singleton = new Lazy<DataSingleton>(() => new DataSingleton(), true);
+
+        List<User> userList;
+        
+        private DataSingleton() 
+        {
+            userList = new List<User>();
+        }
+
+        public static DataSingleton GetInstance()
+        {
+            return _singleton.Value;
+        }
+
+        public void AddUser(User usr)
+        {
+            userList.Add(usr);
+        }
+        public void RemoveUser(User usr)
+        {
+            userList.Remove(usr);
+        }
+
+        public User GetUser(int id)
+        {
+            return userList.Find(User => User.Id == id);
+        }
+
+        public List<User> GetAllUsers()
+        {
+            return new List<User>(userList);
+        }
+
+
+
+    }
+}
