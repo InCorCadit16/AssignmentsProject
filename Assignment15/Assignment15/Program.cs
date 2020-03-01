a74d4e97df2f9edf59e435e8d80589bcd597123a
InCorCadit16 <incorcadit16@gmail.com>
2020-03-02 00:30:47 +0200

    FinishedAssignment 15

--- Assignment15/Assignment15/Program.cs
+++ Assignment15/Assignment15/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Text;
 using System.Linq;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Assignment15
 {
@@ -11,6 +12,13 @@ namespace Assignment15
     {
         static void Main(string[] args)
         {
+            DateTimeAndZones();
+        }
+
+
+        public static void StringsWork()
+        {
+            // Using of encodings 
             Console.Write("Write string with non-Ascii characters: ");
             // π ρ θ î ¶ ¤ ®
             string str = Console.ReadLine();
@@ -22,11 +30,12 @@ namespace Assignment15
                 sw.WriteLine(AsciiStr);
             }
 
+            // String Searching
             if (str.Contains('π'))
-                Console.WriteLine($"string has π with code {(int) 'π'}");
+                Console.WriteLine($"string has π with code {(int)'π'}");
 
             if (str.Contains('ρ'))
-                Console.WriteLine($"string has ρ with code {(int) 'ρ'}");
+                Console.WriteLine($"string has ρ with code {(int)'ρ'}");
 
             if (str.Contains('î'))
                 Console.WriteLine("string has {0} with code {1}", 'î', (int)'î');
@@ -34,16 +43,141 @@ namespace Assignment15
             var cultureInfos = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
             var cultureList = new List<CultureInfo>(cultureInfos);
 
+            // Culture info 
             Console.WriteLine("Choose Culture (two letters)");
             string code = Console.ReadLine().ToUpper();
 
             CultureInfo.CurrentCulture = cultureList.First(culture => culture.CompareInfo.Name.Contains(code));
             Console.WriteLine(CultureInfo.CurrentCulture.TextInfo);
-          
-            Console.WriteLine($"î and i are {("î".Equals("i", StringComparison.CurrentCulture)? "same": "different")} in CurrentCulture");
 
-            Console.WriteLine($"î and i are {("î".Equals("i", StringComparison.InvariantCulture)? "same": "different")} in InvariantCulture");
+            // String comparing
+            Console.WriteLine($"î and i are {("î".Equals("i", StringComparison.CurrentCulture) ? "same" : "different")} in CurrentCulture");
 
+            Console.WriteLine($"î and i are {("î".Equals("i", StringComparison.InvariantCulture) ? "same" : "different")} in InvariantCulture");
+        }
+
+        public static void LastPoint()
+        {
+            /*using (StreamWriter Writer = new StreamWriter("date.txt"))
+            {
+                Writer.WriteLine(DateTime.Now.ToString(), CultureInfo.CurrentCulture);
+            }*/
+
+            string dateStr;
+            using (StreamReader Reader = new StreamReader("date.txt"))
+            {
+                dateStr = Reader.ReadLine();
+            }
+
+            DateTime date = DateTime.Parse(dateStr);
+            Console.WriteLine($"Current culture: {date.ToString(CultureInfo.CurrentCulture)}");
+            Console.WriteLine($"France culture: {date.ToString(CultureInfo.GetCultureInfo(12))}");
+            Console.WriteLine($"Japan culture: {date.ToString(CultureInfo.GetCultureInfo(17))}");
+        }
+
+        public static void FinalizersAndDisposables()
+        {
+            using (var ints = new FixedDataHolder<int>(12))
+            {
+                Random random = new Random();
+                for (int i = 0; i < 12; i++)
+                {
+                    ints[i] = random.Next(1, 500);
+                }
+
+                var Holder = new OneValueHolder<FixedDataHolder<int>>(ints);
+
+                ints.PrintIf(n => n > 250);
+            }
+
+
+        }
+
+        public static void DateTimeAndZones()
+        {
+            Console.Write("Give a number: ");
+            int days = int.Parse(Console.ReadLine());
+
+            var span = new TimeSpan(days, 0, 0, 0);
+            Console.WriteLine($"There will be {DateTime.Now.Add(span).DayOfWeek} after {days} days");
+
+
+            DateTime nowOffset = new DateTime(DateTime.Now.Ticks, DateTimeKind.Unspecified);
+            DateTimeOffset nowInVladivostoc = new DateTimeOffset(nowOffset, TimeSpan.FromHours(8));
+         
+            Console.WriteLine(nowOffset);
+            Console.WriteLine(nowInVladivostoc);
+
+            TimeZoneInfo infoLocal = TimeZoneInfo.Local;
+
+            Console.WriteLine( (infoLocal.SupportsDaylightSavingTime ? "Ooh, dude" : ";D"));
+        }
+    }
+
+    class FixedDataHolder<T> : IDisposable
+    {
+        T[] _data;
+
+        public FixedDataHolder(int size)
+        {
+            _data = new T[size];
+        }
+
+        public FixedDataHolder()
+        {
+            _data = new T[12];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < _data.Length && index >= 0)
+                    return _data[index];
+                else
+                    return default;
+            }
+
+            set
+            {
+                if (index < _data.Length && index >= 0)
+                    _data[index] = value;
+            }
+        }
+
+        public void PrintIf(Func<T, bool> predicate)
+        {
+            foreach (var val in _data)
+            {
+                if (predicate(val)) Console.WriteLine(val);
+            }
+        }
+
+        public void Dispose()
+        {
+            for (int i = 0; i < _data.Length; i++)
+                _data[i] = default;
+
+            _data = null;
+            Console.WriteLine("Data Holder Disposed");
+        }
+    }
+
+    class OneValueHolder<T> where T : new()
+    {
+        public T Value;
+
+        public OneValueHolder(T v)
+        {
+            Value = v;
+        }
+
+        public OneValueHolder() : this(new T()) { }
+
+        ~OneValueHolder() 
+        {
+            Console.WriteLine($"Finalizer called for {Value} value");
+            Value = default;
         }
     }
 }
